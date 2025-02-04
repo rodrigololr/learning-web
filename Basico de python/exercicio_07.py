15973271e6448bf1f9c531c7c821cfe6c5208bf2
Antônio Rodrigo Lima <124109443+rodrigololr@users.noreply.github.com>
2025-02-04 16:02:37 -0300

    Update exercicio_07.py

--- Basico de python/exercicio_07.py
+++ Basico de python/exercicio_07.py
@@ -3,13 +3,19 @@
 
 string = input("Palavra: ")
 
-vogais = ["a", "e", "i", "o", "u"]
+vogais = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
 # vogais = "aeiou"
-
+frase = " " in string
 
 # for(int i; i > string; i++)
 for letra in string:
     if (letra in vogais):
-        print("Rodrigo")
+        print(f" Vogal: {letra}\n")
     else:
-        print("Suco de caju")
+        print("Suco de caju\n")
+
+
+if(frase == 1):
+    print("é uma frase")
+else:
+    print("é uma palavra")
